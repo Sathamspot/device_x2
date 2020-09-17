facc8d04486fff1b21b725729fa0265626e4e827
Shubham <skpawar1305@gmail.com>
2020-09-17 03:28:39 +0000

    X2: remove remaining lineage references

--- devicesettings/Android.mk
+++ devicesettings/Android.mk
@@ -31,7 +31,7 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.preference_preference
 
 LOCAL_STATIC_JAVA_LIBRARIES := \
-    org.lineageos.platform.internal
+#    org.lineageos.platform.internal
 
 LOCAL_RESOURCE_DIR := \
     $(LOCAL_PATH)/res \
